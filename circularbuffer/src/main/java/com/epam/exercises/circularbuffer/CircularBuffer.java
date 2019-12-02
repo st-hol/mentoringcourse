e93c0d503be5c26e62f8c8f51ebc6173316b8e26
Stas <stanislav.holovachuk@gmail.com>
2019-12-02 23:37:00 +0200

    structure for task 1

---
+++ circularbuffer/src/main/java/com/epam/exercises/circularbuffer/CircularBuffer.java
@@ -0,0 +1,50 @@
+package com.epam.exercises.circularbuffer;
+
+import java.util.Comparator;
+import java.util.List;
+
+public class  CircularBuffer <T> implements Buffer <T> {
+
+    public CircularBuffer() {
+    }
+
+    @Override
+    public void put(T t) {
+
+    }
+
+    @Override
+    public T get() {
+        return null;
+    }
+
+    @Override
+    public Object[] toObjectArray() {
+        return new Object[0];
+    }
+
+    @Override
+    public T[] toArray() {
+        return null;
+    }
+
+    @Override
+    public List<T> asList() {
+        return null;
+    }
+
+    @Override
+    public void addAll(List<? extends T> toAdd) {
+
+    }
+
+    @Override
+    public void sort(Comparator<? super T> comparator) {
+
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+}
--- circularbuffer/src/main/java/com/epam/exercises/circularbuffer/Main.java
+++ circularbuffer/src/main/java/com/epam/exercises/circularbuffer/Main.java
@@ -1,4 +1,4 @@
-package com.mentoring;
+package com.epam.exercises.circularbuffer;
 
 public class Main {
     public static void main(String[] args) {
